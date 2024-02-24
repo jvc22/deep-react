0bc7fd0e21465e57a631ed839b783562bc5bfbc9
joaovcostas <jvc22102003@gmail.com>
2024-02-23 23:18:11 -0300

    update: add tabs && btn components and sset max-w to 6xl instead of 1200px

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { Badge } from './ui/badge'
 
 export function Header() {
   return (
-    <div className="max-w-[1200px] mx-auto flex items-center justify-between">
+    <div className="max-w-6xl mx-auto flex items-center justify-between">
       <div className="flex items-center gap-3">
         <div className="flex items-center gap-2.5">
           <img src={nivoLogo} alt="nivo.video" />
