7877d12f1b34ae16d3a5ec2d8b40c7545243ea06
joaovcostas <jvc22102003@gmail.com>
2024-02-24 09:51:01 -0300

    update: add router provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { App } from './app.tsx'
 import './global.css'
 
 const queryClient = new QueryClient()
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
 )
